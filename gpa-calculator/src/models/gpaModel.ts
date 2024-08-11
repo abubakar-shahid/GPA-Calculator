2b61a428e98a2b698c3be8c7bcebedfd17ec8dda
Abubakar Shahid <abubakarshahid1309@gmail.com>
2024-08-12 03:50:55 +0500

    Added gpaModel.ts

---
+++ gpa-calculator/src/models/gpaModel.ts
@@ -0,0 +1,12 @@
+// src/models/gpaModel.ts
+
+export interface Course {
+  course: string;
+  creditHours: number;
+  grade: string;
+}
+
+export interface Semester {
+  semester: string;
+  creditHours: number;
+}
